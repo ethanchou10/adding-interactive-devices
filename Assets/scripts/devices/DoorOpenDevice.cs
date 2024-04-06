4b989111a253cca6408699007da32d38b2190776
Ethan Chou <ethanchou10@outlook.com>
2024-04-06 20:54:15 +1100

    added interactivity

---
+++ Assets/scripts/devices/DoorOpenDevice.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DoorOpenDevice : MonoBehaviour {
+	[SerializeField] private Vector3 dPos;
+
+	private bool _open;
+
+	public void Operate() {
+		if (_open) {
+			Debug.Log("close");
+			Vector3 pos = transform.position - dPos;
+			transform.position = pos;
+		} else {
+			Debug.Log("open");
+			Vector3 pos = transform.position + dPos;
+			transform.position = pos;
+		}
+		_open = !_open;
+	}
+}
