f31153db6ea62e93394e1a154a8f527c83da31df
Ethan Chou <ethanchou10@outlook.com>
2024-04-21 14:29:06 +1000

    removed RelativeMovement

--- Assets/scripts/RelativeMovement.cs
+++ Assets/scripts/RelativeMovement.cs
@@ -11,33 +11,38 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class RelativeMovement : MonoBehaviour {
 	[SerializeField] private Transform target;
-	
-	public float moveSpeed = 6.0f;
 	public float rotSpeed = 15.0f;
+	public float moveSpeed = 6.0f;
+
 	public float jumpSpeed = 15.0f;
 	public float gravity = -9.8f;
-	public float terminalVelocity = -20.0f;
+	public float terminalVelocity = -10.0f;
 	public float minFall = -1.5f;
 
-	private float _vertSpeed;
-	private ControllerColliderHit _contact;
+	public float pushForce = 3.0f;
 
-	private CharacterController _charController;
-	private Animator _animator;
+	private CharacterController charController;
 
-	// Use this for initialization
+	private Animator animator;
+	
+
+	private float vertSpeed;
+	private ControllerColliderHit contact;
+
 	void Start() {
-		_vertSpeed = minFall;
-
-		_charController = GetComponent<CharacterController>();
-		_animator = GetComponent<Animator>();
+		charController = GetComponent<CharacterController>();
+		vertSpeed = minFall;
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update() {
+		
+		
 
 		// start with zero and add movement components progressively
 		Vector3 movement = Vector3.zero;
+		
 
 		// x z movement transformed relative to target
 		float horInput = Input.GetAxis("Horizontal");
@@ -58,51 +63,55 @@ public class RelativeMovement : MonoBehaviour {
 			transform.rotation = Quaternion.Lerp(transform.rotation,
 			                                     direction, rotSpeed * Time.deltaTime);
 		}
-		_animator.SetFloat("Speed", movement.sqrMagnitude);
+		
+		//Debug.Log(movement.sqrMagnitude);
+		animator.SetFloat("Speed", movement.sqrMagnitude);
 
-		// raycast down to address steep slopes and dropoff edge
 		bool hitGround = false;
 		RaycastHit hit;
-		if (_vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit)) {
-			float check = (_charController.height + _charController.radius) / 1.9f;
-			hitGround = hit.distance <= check;	// to be sure check slightly beyond bottom of capsule
+		if (vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit)) {
+			float check = (charController.height + charController.radius) / 1.9f;
+			hitGround = hit.distance <= check;
 		}
 
-		// y movement: possibly jump impulse up, always accel down
-		// could _charController.isGrounded instead, but then cannot workaround dropoff edge
 		if (hitGround) {
 			if (Input.GetButtonDown("Jump")) {
-				_vertSpeed = jumpSpeed;
+				vertSpeed = jumpSpeed;
 			} else {
-				_vertSpeed = minFall;
-				_animator.SetBool("Jumping", false);
+				vertSpeed = minFall;
+				animator.SetBool("Jumping", false);
 			}
 		} else {
-			_vertSpeed += gravity * 5 * Time.deltaTime;
-			if (_vertSpeed < terminalVelocity) {
-				_vertSpeed = terminalVelocity;
+			vertSpeed += gravity * 5 * Time.deltaTime;
+			if (vertSpeed < terminalVelocity) {
+				vertSpeed = terminalVelocity;
 			}
-			if (_contact != null ) {	// not right at level start
-				_animator.SetBool("Jumping", true);
+			if (contact != null) {
+				animator.SetBool("Jumping", true);
 			}
-
-			// workaround for standing on dropoff edge
-			if (_charController.isGrounded) {
-				if (Vector3.Dot(movement, _contact.normal) < 0) {
-					movement = _contact.normal * moveSpeed;
+			if (charController.isGrounded) {
+				if (Vector3.Dot(movement, contact.normal) < 0) {
+					movement = contact.normal * moveSpeed;
 				} else {
-					movement += _contact.normal * moveSpeed;
+					movement += contact.normal * moveSpeed;
 				}
 			}
 		}
-		movement.y = _vertSpeed;
+
+
+		
+		movement.y = vertSpeed;
 
 		movement *= Time.deltaTime;
-		_charController.Move(movement);
+		charController.Move(movement);
 	}
 
-	// store collision to use in Update
 	void OnControllerColliderHit(ControllerColliderHit hit) {
-		_contact = hit;
+		contact = hit;
+
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (body != null && !body.isKinematic) {
+			body.velocity = hit.moveDirection * pushForce;
+		}
 	}
 }
